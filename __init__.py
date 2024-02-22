079707f8c406717a658d2d4fc2a37fb10417ee7f
Colter Cotten <58351939+cottensmcc@users.noreply.github.com>
2024-02-22 14:39:53 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
 import check50
 
 @check50.check()
+def exists():
+  """exists"""
+  check50.exists("./a.out")
+
+@check50.check()
 def add():
   """15"""
   check50.run("./a.out").stdout("15", regex=False).exit(0)
+
+
